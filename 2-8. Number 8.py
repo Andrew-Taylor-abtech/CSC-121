8d68a8c5bd0eb42f78828dcb7c9bc82594002d92
Drew Taylor <andrewtaylor02@students.abtech.edu>
2026-01-31 14:39:41 -0500

    renamed:    name_cases.py -> module_2/2-4. Name Cases.py 	renamed:    2-8. Number 8.py -> module_2/2-8. Number 8.py

--- 2-8. Number 8.py
+++
@@ -1,9 +0,0 @@
-# This program performs four basic arithmetic operations resulting in the number 8.
-#addition
-print(2+6)
-#subtraction
-print(100-92)
-#multiplication
-print(8*1)
-#division
-print(32/4)
---
+++ module_2/2-4. Name Cases.py
@@ -0,0 +1,6 @@
+first_name = "drew"
+last_name = "taylor"
+full_name = f"{first_name} {last_name}"
+print(full_name.lower())
+print(full_name.upper())
+print(full_name.title())
