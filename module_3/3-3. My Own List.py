89df45d39ec36f2ace462358a19bc37628964fd6
Drew Taylor <andrewtaylor02@students.abtech.edu>
2026-02-09 20:49:33 -0500

    new file:   module_3/3-3. My Own List.py 	new file:   module_3/3-8. Guest Lists Sorted.py

---
+++ module_3/3-3. My Own List.py
@@ -0,0 +1,5 @@
+trains_are_the_superior_mode_of_transportation = ['bullet trains', 'freight trains', 'trains of thought']
+
+print(f"\n{trains_are_the_superior_mode_of_transportation[0].title()} are the best way to travel in other countries.")
+print(f"{trains_are_the_superior_mode_of_transportation[1].title()} are essential for transporting goods across countries, even our own.")
+print(f"{trains_are_the_superior_mode_of_transportation[2].title()} aren't even modes of transportation, oh no! we love them still.")
---
+++ module_3/3-8. Guest Lists Sorted.py
@@ -0,0 +1,21 @@
+guest_list = ['Groucho Marx', 'Anthony Bourdain', 'David Attenborough', 'Neil deGrasse Tyson']
+
+print(f"\n{guest_list[0]} you are cordially invited to the dinner of all dinners.")
+print(f"{guest_list[1]} you are cordially invited to dinner of all dinners.")
+print(f"{guest_list[2]} you are cordially invited to dinner of all dinners.")
+print(f"{guest_list[3]} you are cordially invited to dinner of all dinners.")
+
+print("\nGreat Scott! We found a bigger dinner table! Quick, who can we invite?")
+print('the party thought for a long moment...')
+
+guest_list.insert(0, 'Elmo')
+guest_list.append('Big Bird')
+guest_list.sort() 
+
+print(f"\n{guest_list[0]} you are cordially invited to dinner of all dinners.")
+print(f"{guest_list[1]} you are cordially invited to dinner of all dinners.")
+print(f"{guest_list[2]} you are cordially invited to dinner of all dinners.")
+print(f"{guest_list[3]} you are cordially invited to dinner of all dinners.")
+print(f"{guest_list[4]} you are cordially invited to dinner of all dinners.")
+print(f"{guest_list[5]} you are cordially invited to dinner of all dinners.")
+print()
