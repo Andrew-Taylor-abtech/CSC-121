eb7951aeab83c5aa3c615345a63dbbab3fe4f130
Drew Taylor <andrewtaylor02@students.abtech.edu>
2026-02-14 17:03:25 -0500

    new file:   module_4/4-10 Slices.py 	new file:   module_4/4-15_animals.py 	new file:   module_4/4-15_even_numbers.py 	new file:   module_4/4-15_slices.py 	new file:   module_4/animals.py

---
+++ module_4/4-15_animals.py
@@ -0,0 +1,4 @@
+animals = ('python', 'cobra', 'garter', 'viper')
+for animal in animals:
+    print(f"A {animal.title()} slithers around like a freaking danger noodle.")
+print ('\n snakes really do be sneaking around all over')
---
+++ module_4/4-15_even_numbers.py
@@ -0,0 +1,4 @@
+players = ('charles', 'martina', 'michael', 'florence', 'eli')
+print("Here are the last 4 players on my team:")
+for player in players[-4:]:
+    print(player.title())
